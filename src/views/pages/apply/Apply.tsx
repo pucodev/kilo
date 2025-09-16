af4fc814879067607892326486fec67217adb2ad
Jorge Estrada <jor1550g@gmail.com>
2025-09-16 15:28:22 -0500

    (apply) feat: add zones to form

--- src/views/pages/apply/Apply.tsx
+++ src/views/pages/apply/Apply.tsx
@@ -1,4 +1,5 @@
 import SelectZoneDialog from '@components/apply/SelectZoneDialog'
+import type ZoneModel from '@models/zone.model'
 import { Button } from '@pucoui/Button'
 import {
   Card,
@@ -11,12 +12,28 @@ import {
 import { Field, FieldLabel } from '@pucoui/Field'
 import { Input } from '@pucoui/Input'
 import applog from '@utils/logger'
+import { useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
 
 export default function Apply() {
   const [searchParams] = useSearchParams()
   const selectedZone = Number(searchParams.get('zone') || 0)
+  const [zones, setZones] = useState<ZoneModel[]>([])
+  const [isShowDialog, setIsShowDialog] = useState(false)
   applog.debug('Selected Zone = ', selectedZone)
+
+  function onAddZone(zone: ZoneModel) {
+    // Do not add zone if is already on list
+    if (!zones.some(item => item.id === zone.id)) {
+      setZones(prev => [...prev, zone])
+    }
+
+    setIsShowDialog(false)
+  }
+
+  function removeZone(id: number) {
+    setZones(prev => prev.filter(item => item.id !== id))
+  }
 
   return (
     <div>
@@ -30,29 +47,42 @@ export default function Apply() {
         <Field>
           <FieldLabel>Zones</FieldLabel>
           <div className="is-hstack">
-            {/* <Card style={{ maxWidth: '320px' }}> */}
-            {/*   <CardImage className="is-16by9" /> */}
-            {/*   <CardBody> */}
-            {/*     <CardTitle>Name</CardTitle> */}
-            {/*     <CardContent>Description</CardContent> */}
-            {/*   </CardBody> */}
-            {/**/}
-            {/*   <CardActions> */}
-            {/*     <Button outlined>Remove</Button> */}
-            {/*   </CardActions> */}
-            {/* </Card> */}
-            {/**/}
-            <Button
-              outlined
-              style={{
-                borderStyle: 'dashed',
-                borderWidth: '2px',
-                width: '100%',
-                maxWidth: '320px',
-              }}
-            >
-              Add zone
-            </Button>
+            {zones.map(zone => (
+              <Card key={zone.id} style={{ maxWidth: '320px' }}>
+                <CardImage src={zone.imageSrc} className="is-16by9" />
+                <CardBody>
+                  <CardTitle>{zone.name}</CardTitle>
+                  <CardContent>{zone.description}</CardContent>
+                </CardBody>
+
+                <CardActions>
+                  <Button
+                    outlined
+                    color="error"
+                    onClick={() => removeZone(zone.id)}
+                  >
+                    Remove
+                  </Button>
+                </CardActions>
+              </Card>
+            ))}
+
+            {zones.length < 3 ? (
+              <Button
+                outlined
+                style={{
+                  borderStyle: 'dashed',
+                  borderWidth: '2px',
+                  width: '100%',
+                  maxWidth: '320px',
+                }}
+                onClick={() => setIsShowDialog(true)}
+              >
+                Add zone
+              </Button>
+            ) : (
+              <></>
+            )}
           </div>
         </Field>
         <Field>
@@ -66,7 +96,11 @@ export default function Apply() {
         <Button>Apply</Button>
       </div>
 
-      <SelectZoneDialog></SelectZoneDialog>
+      {isShowDialog ? (
+        <SelectZoneDialog onSelect={onAddZone}></SelectZoneDialog>
+      ) : (
+        <></>
+      )}
     </div>
   )
 }
